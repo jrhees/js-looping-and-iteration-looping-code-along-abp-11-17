5a388d78461ee4b4da962b83e3fb017c0c1a6297
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-10 16:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,3 @@ function printBadges(employees) {
   return employees;
 }
 
-function tailsNeverFails() {
-  let tails = 0;
-  while (Math.random() >= 0.5) {
-    tails +=1;
-  }
-  return `You got ${tails} tails in a row!`;
-}
